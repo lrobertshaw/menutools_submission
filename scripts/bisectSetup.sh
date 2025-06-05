4cf444734259ed497a8c496bf7c286539cc36d01
Robert Ward <robert.james.ward@cern.ch>
2025-06-05 21:24:32 +0200

    Update bisect to use 151pre3 as default

--- scripts/bisectSetup.sh
+++ scripts/bisectSetup.sh
@@ -1,4 +1,4 @@
 SHORTREL=$1
 
-cp submit_v45wTT_150pre3.yaml submit_v45wTT_$SHORTREL.yaml
-sed -i "s/150pre3/$SHORTREL/g" submit_v45wTT_$SHORTREL.yaml
+cp submit_v45wTT_151pre3.yaml submit_v45wTT_$SHORTREL.yaml
+sed -i "s/151pre3/$SHORTREL/g" submit_v45wTT_$SHORTREL.yaml
