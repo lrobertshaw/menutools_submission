7101135e4110a6ff7d2b816d1ea4f9e890b0a750
Robert Ward <robert.james.ward@cern.ch>
2025-06-06 16:58:31 +0200

    Backup cmsDriver and cmsRun commands directly in repo

---
+++ cmsrun/remakewTT_v45_withGen_D121.sh
@@ -0,0 +1,4 @@
+#NOTE: put threads=4 here which differs to testing (and n=40 to check it works!)
+INPUT=/store/mc/Phase2Spring24DIGIRECOMiniAOD/TT_TuneCP5_14TeV-powheg-pythia8/GEN-SIM-DIGI-RAW-MINIAOD/PU200_AllTP_140X_mcRun4_realistic_v4-v1/2560000/11d1f6f0-5f03-421e-90c7-b5815197fc85.root
+
+cmsDriver.py l1nanoPhase2 -s L1TrackTrigger,L1,L1P2GT,NANO:@Phase2L1DPGwithGen --conditions auto:phase2_realistic_T33 --geometry ExtendedRun4D121 --era Phase2C17I13M9 --eventcontent NANOAOD --datatier GEN-SIM-DIGI-RAW-MINIAOD --customise SLHCUpgradeSimulations/Configuration/aging.customise_aging_1000,Configuration/DataProcessing/Utils.addMonitoring,L1Trigger/Configuration/customisePhase2TTOn110.customisePhase2TTOn110 --filein $INPUT --fileout file:output_Phase2_L1T.root --python_filename v45_rerunL1wTT_withGen_D121_cfg.py --inputCommands="keep *, drop l1tPFJets_*_*_*, drop l1tTrackerMuons_l1tTkMuonsGmt*_*_HLT" --mc -n 40 --nThreads 4
