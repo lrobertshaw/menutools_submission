a64e9549af3365c46545c59f30503a2ab9d658cb
Robert Ward <robert.james.ward@cern.ch>
2025-06-15 23:34:22 +0200

    Add extra info to log summary

--- scripts/checkMultiple.sh
+++ scripts/checkMultiple.sh
@@ -1,7 +1,12 @@
 # joblist="151X_D110 151X_D116 151X_D121 151pre3_only48136 151pre3_SC8Nano"
 # joblist="151pre3_L1EGupdate1"
-joblist="151pre3_E2ENNVtx 151pre3_E2ENNVtxOff 151X_E2ENNVtx 151X_L1EGupdate2"
-revision=1am
+# joblist="151pre3_E2ENNVtx 151pre3_E2ENNVtxOff 151X_E2ENNVtx 151X_L1EGupdate2"
+# joblist="151X_E2ENNVtx 151X_L1EGupdate2 151pre3_L1EGupdate3 151pre3_E2ENNVtxOnlyFind 151pre3_E2ENNVtxOnlyAssoc"
+# joblist="151X_E2ENNVtx 151X_L1EGupdate2 151pre3 151pre3_E2ENNVtxOnlyFind 151pre3_E2ENNVtxOnlyAssoc"
+# joblist="151pre3"
+joblist="151pre3_DispVtx 151pre1 151pre3_L1EGupdate4 151X_MergedAR24_FindOff 151X_MergedAR24_FindOn 151X_AllAR25_FindOff 151X_AllAR25_FindOn"
+
+revision=11pm
 RESUBMIT=$1
 
 for job in $joblist; do
@@ -22,6 +27,9 @@ else
 	grep -r "finished" logs/check_${job}_${revision}.log
 	grep -r "   failed" logs/check_${job}_${revision}.log
 	grep -r "running" logs/check_${job}_${revision}.log
+	grep -r "transferring" logs/check_${job}_${revision}.log
+	grep -r "rescheduled" logs/check_${job}_${revision}.log
+	grep -r "idle" logs/check_${job}_${revision}.log
     done
 fi
 
